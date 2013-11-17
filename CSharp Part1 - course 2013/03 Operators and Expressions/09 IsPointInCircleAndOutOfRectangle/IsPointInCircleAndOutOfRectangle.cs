6369d357c39a3d5369e3b0fd6320a418e96ca400
ipobornik <gereg.sith@gmail.com>
2013-11-17 17:14:01 +0200

    03 Operators and Expressions v3

--- CSharp Part1 - course 2013/03 Operators and Expressions/09 IsPointInCircleAndOutOfRectangle/IsPointInCircleAndOutOfRectangle.cs
+++ CSharp Part1 - course 2013/03 Operators and Expressions/09 IsPointInCircleAndOutOfRectangle/IsPointInCircleAndOutOfRectangle.cs
@@ -4,21 +4,24 @@
 /*
 09. Write an expression that checks for given point (x, y) if it is within the circle K( (1,1), 3) and out of the rectangle R(top=1, left=-1, width=6, height=2).
 
-ОБЯСНЕНИЕ: Задача подобна на 6-та.
-
+ОБЯСНЕНИЕ: В тази задача трябва да проверим дали кординатите x и y се намират едновременно в дадената окръжност и същевременно извън дадения правоъгълник. 
+За целта ще трябва да преизползваме формулата от 6-та задача за намиране на точка в окръжност: x² + y² < r² и да добавим формула, която да проверява дали координатите са извън правоъгълника.
+ 
  */
 
 class IsPointInCircleAndOutOfRectangle
 {
     static void Main()
     {
-        int px = 0, py = 2;
-        int kx = 1, ky = 1, kr = 3;
-        int rx = -1, ry = 1, rw = 6, rh = 2;
 
-        bool isInCircle = Math.Pow(px - kx, 2) + Math.Pow(py - ky, 2) < Math.Pow(kr, 2);
-        bool isOutOfRectangle = px < rx || px > rx + rw || py > ry || py < ry - rh;
+        Console.Write("Please enter X coordinate: ");
+        decimal x = decimal.Parse(Console.ReadLine());
 
-        Console.WriteLine("In circle and out of rectangle: {0}", (isInCircle && isOutOfRectangle));
+        Console.Write("Please enter Y coordinate: ");
+        decimal y = decimal.Parse(Console.ReadLine());
+        Console.WriteLine();
+
+        bool result = (((x - 1)*(x - 1) + (y - 1)*(y - 1)) <= 9) && ((x < -1) || ((x >= -1) && ((y <= -1) || (y >= 1))));
+        Console.WriteLine("The coordinates are inside the circle and out of the rectangular: {0}", result);
     }
 }
